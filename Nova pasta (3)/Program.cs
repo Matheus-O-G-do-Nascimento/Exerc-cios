2070383b04bf685894cccf808280c2503a4522b8
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-07-02 04:28:13 -0300

    Add files via upload

---
+++ Nova pasta (3)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Nova_pasta__3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento;
+            int anoAtual;
+            int idade;
+
+            Console.WriteLine("Por favor,insira o ano de seu nascimento abaixo: ");
+            anoNascimento = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Agora, por favor insira o ano atual abaixo:");
+            anoAtual = Convert.ToInt32(Console.ReadLine());
+            idade = anoAtual-anoNascimento;
+            Console.WriteLine("Sua idade é de " + idade +" anos, ou seja aproximadamente " + idade*52 + " semanas.");
+        }
+    }
+}
