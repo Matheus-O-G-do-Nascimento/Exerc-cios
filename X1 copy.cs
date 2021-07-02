315d322949e69d6431e7d38fb815912bec339202
Matheus O.G. do Nascimento <82417324+Matheus-O-G-do-Nascimento@users.noreply.github.com>
2021-07-02 04:21:41 -0300

    Ex1

---
+++ X1 copy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoNascimento;
+            int anoAtual;
+            int idade;
+
+            idade = anoAtual-anoNascimento;
+
+            Console.WriteLine("Por favor,insira o ano de seu nascimento abaixo: ");
+            anoNascimento = Console.ReadLine();
+            Console.WriteLine(" Agora, por favor insira o ano atual abaixo:");
+            anoAtual = Console.ReadLine();
+            Console.WriteLine("Sua idade é de " + idade +" anos, ou seja aproximadamente " + idade*52 + " semanas.");
+        }
+    }
+}
---
+++ X1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            int meses; 
+            int dias;
+            int horas;
+            int minutos;
+            
+            Console.WriteLine("Qual é a sua idade?");
+            idade =  Convert.ToInt32(Console.ReadLine());
+            meses = idade*12;
+            dias = meses*30;
+            horas = dias*24;
+            minutos = horas*60;
+            Console.WriteLine("Então você tem "+ idade + " anos, o que é equivalente a "+ meses + " meses ou "+ dias + " dias ou "+ horas + " horas ou " + minutos + " minutos!!");
+        }
+    }
+}
